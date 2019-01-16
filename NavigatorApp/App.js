32e4c2c27a8a1f3d6e5a02c0c48b3ecd809c638f
Yolk Huang <yolk.tw@gmail.com>
2019-01-16 15:26:21 +0800

    Add 'going back' events on 'Go to Home', 'Go back' button

--- NavigatorApp/App.js
+++ NavigatorApp/App.js
@@ -22,8 +22,16 @@ class DetailsScreen extends React.Component {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Details Screen</Text>
                 <Button
-                    title="Go to Details... again"
+                    title='Go to Details... again'
                     onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button 
+                    title='Go to Home'
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button 
+                    title='Go back'
+                    onPress={() => this.props.navigation.goBack()}
                 />
             </View>
         );
